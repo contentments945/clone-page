f3885731fb05ab01a9e57423e9903f4b68a9b738
contentments945 <kceacontentments1@gmail.com>
2026-01-04 16:53:06 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl=gsap.timeline()
+function loadingAnimation(){
+    var tl=gsap.timeline()
 tl.from(".line h1",{
     y:150,
     stagger:0.25,
@@ -29,15 +30,39 @@ tl.to('.line h2',{
 tl.to("#loader",{
     opacity:0,
     duration:0.2,
-    delay:4,
+    delay:0,
 });
 tl.from("#page1",{
     delay:0.2,
     y:1600,
-    duration:0.5, 
+    duration:0.6, 
     opacity:0,
-    ease:power4
+    ease:"power4.out"
 })
 tl.to("#loader",{
     display:"none"
 })
+
+tl.from("#nav",{
+    opacity:0
+});
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120, 
+    stagger:0.2
+});
+
+}
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+    gsap.to("#crsr",{
+        left:dets.x,
+        top:dets.y
+    })
+
+});     
+Shery.makeMagnet("#nav-part2 h3");  
+}
+loadingAnimation();
+cursorAnimation()
+
